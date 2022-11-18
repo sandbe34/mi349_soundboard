780cc8922a1f0b4bbe9dcae8894acf27adff0ed3
sandbe34 <sandbe34@msu.edu>
2022-11-18 16:14:04 -0500

    created soundboard and imported all audio files

---
+++ soundboard.js
@@ -0,0 +1,75 @@
+//button1
+var buttonElement = document.getElementById('button1')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+})
+
+//button2
+var buttonElement = document.getElementById('button2')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+})
+
+//button3
+var buttonElement = document.getElementById('button3')
+buttonElement.addEventListener('click', function () {
+  console.log('I was clicked!')
+})
+
+
+//sound1
+function playsound1() {
+  var soundclip=document.getElementById("audioclip1");
+  soundclip.play();
+}
+
+//sound2
+function playsound2() {
+  var soundclip=document.getElementById("audioclip2");
+  soundclip.play();
+}
+
+//sound3
+function playsound3() {
+  var soundclip=document.getElementById("audioclip3");
+  soundclip.play();
+}
+
+//sound4
+function playsound4() {
+  var soundclip=document.getElementById("audioclip4");
+  soundclip.play();
+}
+
+//sound5
+function playsound5() {
+  var soundclip=document.getElementById("audioclip5");
+  soundclip.play();
+}
+
+//sound6
+function playsound6() {
+  var soundclip=document.getElementById("audioclip6");
+  soundclip.play();
+}
+
+//sound7
+function playsound7() {
+  var soundclip=document.getElementById("audioclip7");
+  soundclip.play();
+}
+
+//sound8
+function playsound8() {
+  var soundclip=document.getElementById("audioclip8");
+  soundclip.play();
+}
+
+//sound9
+function playsound9() {
+  var soundclip=document.getElementById("audioclip9");
+  soundclip.play();
+}
+
+//opening message
+alert('Welcome to Patrick\'s Soundboard! \nClick on a sound to hear what\'s behind it.')
